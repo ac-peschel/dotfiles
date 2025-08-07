01ffdd3c4bcccca5c14f11ecf5fa4975addffcfc
ac-peschel <mail@acpeschel.net>
2025-08-07 22:45:57 +0200

    switched to kitty

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@ link_config() {
    ln -sf "$1" "$2"
 }
 
-link_config ~/repos/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 link_config ~/repos/dotfiles/i3/config ~/.config/i3/config
 link_config ~/repos/dotfiles/init.lua ~/.config/nvim/init.lua
 link_config ~/repos/dotfiles/settings.json ~/.config/Code/User/settings.json
 link_config ~/repos/dotfiles/.tmux.conf ~/.tmux.conf
 link_config ~/repos/dotfiles/config.yml ~/.config/lazygit/config.yml
+link_config ~/repos/dotfiles/kitty.conf ~/.config/kitty/kitty.conf
