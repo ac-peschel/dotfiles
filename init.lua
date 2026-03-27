df778e21663c3fabe2fdade8f4a1a5222d9860e8
ac-peschel <mail@acpeschel.net>
2026-03-27 22:56:36 +0100

    swap to nvim for go

---
+++ init.lua
@@ -0,0 +1,172 @@
+-- Basic config
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number relativenumber")
+vim.o.wrap = false
+vim.g.mapleader = " "
+vim.keymap.set("n", "<Esc>", "<Cmd>nohlsearch<CR>", { silent = true })
+vim.opt.guicursor = ""
+vim.g.clipboard = {
+	name = "xclip",
+	copy = { ["+"] = "xclip -selection clipboard", ["*"] = "xclip -selection primary" },
+	paste = { ["+"] = "xclip -selection clipboard -o", ["*"] = "xclip -selection primary -o" },
+	cache_enabled = 0,
+}
+vim.keymap.set("n", "<C-v>", '"+p', {})
+vim.keymap.set("v", "<C-c>", '"+y', {})
+
+-- LSP
+local function on_attach(client, bufnr)
+  local opts = { noremap = true, silent = true, buffer = true }
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_actions, opts)
+  if client.server_capabilities.documentFormattingProvider then
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ async = true })
+      end
+    })
+  end
+end
+local function attach_lsp(pattern, name, cmd, settings)
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = pattern,
+    callback = function()
+      local bufnr = args.buf
+      local clients = vim.lsp.get_active_clients({bufnr=bufnr})
+      for _, c in ipairs(clients) do
+        if c.name == name then
+          return
+        end
+      end
+      local root = vim.fs.dirname(vim.fs.find({"go.mod", ".git"}, {upward=true})[1])
+      local client_id = vim.lsp.start({
+        name = name,
+        cmd = cmd,
+        root_dir = root,
+        settings = settings,
+      })
+      vim.lsp.buf_attach_client(bufnr, client_id)
+      local client = vim.lsp.get_client_by_id(client_id)
+      on_attach(client, bufnr)
+    end
+  })
+end
+attach_lsp({"go", "gomod"}, "gopls", {"gopls"})
+
+-- Plugins
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", 
+    "clone", 
+    "--filter=blob:none", 
+    "https://github.com/folke/lazy.nvim.git", 
+    "--branch=stable", 
+    lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+  {
+    "rebelot/kanagawa.nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme kanagawa-dragon")
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.config").setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          preview = {
+            treesitter = false
+          }
+        }
+      })
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    end
+  },
+  {
+    "stevearc/oil.nvim",
+    config = function()
+      require("oil").setup({
+        columns = { "icon", "permissions", "size", "mtime" },
+        view_options = { show_hidden = true },
+        keymaps = {
+          ["<CR>"] = "actions.select",
+          ["-"] = "actions.parent",
+          ["q"] = "actions.close",
+        }
+      })
+      vim.keymap.set("n", "<leader>e", "<Cmd>Oil<CR>", {})
+    end
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup({
+        check_ts = true,
+        disable_filetypes = { "TelescopePrompt", "vim" }
+      })
+    end
+  },
+  {
+    "preservim/nerdcommenter"
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    lazy = false,
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C.Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({select = true})
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+        })
+      })
+    end
+  }
+})
