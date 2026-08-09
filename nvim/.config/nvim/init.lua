770721bf7926535f8e55cb44ddcc96ee1121d309
ac-peschel <mail@acpeschel.net>
2026-08-09 23:42:39 +0200

    added dotfiles in stow friendly pattern

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,88 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=3")
+vim.cmd("set softtabstop=3")
+vim.cmd("set shiftwidth=3")
+vim.cmd("set number relativenumber")
+vim.opt.guicursor = ""
+vim.o.wrap = false
+vim.g.mapleader = " "
+vim.keymap.set("n", "<Esc>", "<Cmd>nohlsearch<CR>", {})
+vim.keymap.set("n", "<C-c>", '"+y', {})
+vim.keymap.set("n", "<C-v>", '"+p', {})
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+   vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable",
+      lazypath
+   })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+   {
+      "rebelot/kanagawa.nvim",
+      priority = 1000,
+      config = function()
+         vim.cmd("colorscheme kanagawa-dragon")
+      end
+   },
+   {
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+      config = function()
+         require("nvim-treesitter.config").setup({
+            highlight = { enable = true },
+            indent = { enable = true },
+         })
+      end
+   },
+   {
+      "nvim-telescope/telescope.nvim",
+      tag = "0.1.8",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      config = function()
+         require("telescope").setup({
+            defaults = {
+               preview = {
+                  treesitter = false
+               }
+            }
+         })
+         local builtin = require("telescope.builtin")
+         vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+         vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      end
+   },
+   {
+      "stevearc/oil.nvim",
+      config = function()
+         require("oil").setup({
+            columns = { "icon", "permissions", "size", "mtime" },
+            view_options = { show_hidden = true },
+            keymaps = {
+               ["<CR>"] = "actions.select",
+               ["-"] = "actions.parent",
+               ["q"] = "actions.close",
+            },
+         })
+         vim.keymap.set("n", "<leader>e", "<Cmd>Oil<CR>", {})
+      end
+   },
+   {
+      "windwp/nvim-autopairs",
+      event = "InsertEnter",
+      config = function()
+         require("nvim-autopairs").setup({
+            check_ts = true,
+            disable_filetypes = { "TelescopePrompt", "vim" },
+         })
+      end
+   },
+   {
+      "preservim/nerdcommenter"
+   },
+})
